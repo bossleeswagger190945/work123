d46ffc6dd7207027b9beaf3f7a75c6c3133d1d40
bossleeswagger190945 <68529231+bossleeswagger190945@users.noreply.github.com>
2020-07-20 17:53:57 +0700

    Add files via upload

---
+++ work-1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"1st Term of 2020\t"<<"\\45132114   Wanthanee Prachuabsupakiq"<<endl;
+	cout<<"-------------------------------------------------------------------"<<endl;
+	cout<<"Subject ID | Subject name\t | Score (100) | Grad |"<<endl;
+	cout<<"-------------------------------------------------------------------"<<endl;
+	cout<<"4570034   | Introduction to computer  |   85   | ""\"A""\" |"<<endl;
+	cout<<"4580247   | English                   |   85   | ""\"B""\" |"<<endl;
+	cout<<"4580247   | Design Thinking           |   85   | ""\"C""\" |"<<endl;
+	cout<<"-------------------------------------------------------------------"<<endl;
+	cout<<"4580247   | Subject = 3               | Average = 3.87 |"<<endl;
+	cout<<"====================================================="<<endl;
+	cout<<""<<endl;
+	cout<<""<<endl;
+	cout<<"KMUTNB"<<endl;
+return(0);
+}
